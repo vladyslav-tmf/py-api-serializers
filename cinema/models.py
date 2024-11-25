2cd69a414d7874d450d10eae5bf5d61fb0530389
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-25 14:23:55 +0200

    Refactor validation error message formatting
    - Refactored raising a validation error in "models.py" to avoid flake8 error.

--- cinema/models.py
+++ cinema/models.py
@@ -99,10 +99,12 @@ class Ticket(models.Model):
             if not (1 <= ticket_attr_value <= count_attrs):
                 raise ValidationError(
                     {
-                        ticket_attr_name: f"{ticket_attr_name} number "
-                                          "must be in available range: "
-                                          f"(1, {cinema_hall_attr_name}): "
-                                          f"(1, {count_attrs})"
+                        ticket_attr_name: (
+                            f"{ticket_attr_name} number "
+                            "must be in available range: "
+                            f"(1, {cinema_hall_attr_name}): "
+                            f"(1, {count_attrs})"
+                        )
                     }
                 )
 

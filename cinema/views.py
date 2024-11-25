ae73f27452f66c8577a0966e8b60a6e3c0e34185
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-25 14:18:55 +0200

    Optimize Movie and MovieSession viewsets
    - Added type hints to Movie and MovieSession methods.
    - Optimized queryset handling in "get_queryset" methods by adding "prefetch_related" and "select_related".

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,8 @@
+from typing import Type
+
+from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.serializers import BaseSerializer
 
 from cinema.models import Actor, CinemaHall, Genre, Movie, MovieSession
 from cinema.serializers import (
@@ -32,20 +36,34 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[BaseSerializer]:
         if self.action == "list":
             return MovieListSerializer
         elif self.action == "create":
             return MovieCreateSerializer
         return MovieSerializer
 
+    def get_queryset(self) -> QuerySet[Movie]:
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"]:
+            return queryset.prefetch_related("genres", "actors")
+        return queryset
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[BaseSerializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         elif self.action == "create":
             return MovieSessionCreateSerializer
         return MovieSessionSerializer
+
+    def get_queryset(self) -> QuerySet[MovieSession]:
+        queryset = self.queryset
+        if self.action == "list":
+            return queryset.select_related()
+        elif self.action == "retrieve":
+            return queryset.prefetch_related("movie__genres", "movie__actors")
+        return queryset
